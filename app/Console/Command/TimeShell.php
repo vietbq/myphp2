9bdf381b2f73f1d2c0d7760624855d0bf42883bd
buiquocviet <buiquocviet1993@gmail.com>
2014-07-31 17:07:37 +0700

    update

---
+++ app/Console/Command/TimeShell.php
@@ -0,0 +1,24 @@
+<?php
+
+class Timeshell extends AppShell {
+   
+    public function main() {
+        
+	$conn=mysql_connect("localhost", "myphp", "12345678") or die("can't connect database");
+        mysql_select_db("myphp",$conn);
+      
+
+        $link = "http://tuyennq.codelovers.vn:8080/time.php";
+        $html = file_get_contents($link);
+        $this->out($html);
+        $time = date("YmdHis", (int) $html);
+       // $time = gmdate((int) $html);
+        $this->out($time);
+       
+        $sql = "INSERT INTO  time_shell (time) VALUES ($time)";
+         mysql_query($sql);
+
+    } 
+    
+}
+?>
